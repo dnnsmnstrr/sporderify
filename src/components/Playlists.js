90deeab6df22a5596bb03f11f89a8ac439d44f43
Dennis Muensterer <dennismuensterer@gmail.com>
2021-01-25 00:23:16 +0100

    Replace playlist in chunks to support over 100 tracks

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -12,17 +12,24 @@ const Playlists = ({token}) => {
   const { data } = useUser()
   const [selectedPlaylist, setSelectedPlaylist] = useState('')
   const [isSorting, setIsSorting] = useState(false)
+
   const spotifyApi = new Spotify()
   spotifyApi.setAccessToken(token)
 
-  useEffect(() => {
-    // spotifyApi.skipToNext()
-  }, [])
+  const splitToChunks = (arr, size) => arr.reduce((acc, e, i) => (i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc), []);
 
   const reorderPlaylist = async (direction = 'asc') => {
     if (selectedPlaylist && selectedPlaylist.id) {
-      const tracks = await spotifyApi.getPlaylistTracks(selectedPlaylist.id)
-      const sortedTracks = [...tracks.items].sort((a, b) => {
+      let offset = 0
+      let allTracks = []
+      let reached
+      while (allTracks.length < selectedPlaylist.tracks.total) {
+        const tracks = await spotifyApi.getPlaylistTracks(selectedPlaylist.id, {offset})
+        allTracks = allTracks.concat(tracks.items)
+        console.log('tracks', tracks, allTracks)
+        offset += 100
+      }
+      const sortedTracks = allTracks.sort((a, b) => {
         console.log('direction', direction, a, b)
         if (direction === 'asc') {
           return a.track.name.length - b.track.name.length
@@ -32,7 +39,13 @@ const Playlists = ({token}) => {
       const sortedTitles = sortedTracks.map(({track: {name}}) => name)
       console.log('sortedTitles', sortedTitles)
       const sortedUris = sortedTracks.map(({track: {uri}}) => uri)
-      await spotifyApi.replaceTracksInPlaylist(selectedPlaylist.id, sortedUris)
+      const chunks = splitToChunks(sortedUris, 100)
+      //replace playlist with first chunk of tracks
+      await spotifyApi.replaceTracksInPlaylist(selectedPlaylist.id, chunks.shift())
+
+      for (var chunk of chunks) {
+        await spotifyApi.addTracksToPlaylist(selectedPlaylist.id, chunk)
+      }
     }
   }
 
